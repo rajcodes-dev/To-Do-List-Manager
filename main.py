56f7c168ecfbe2855e37cf91fce209e806734513
rajcodes-dev <rajtiwari2006t@gmail.com>
2026-01-10 10:48:15 +0530

    added to-do_list

---
+++ main.py
@@ -0,0 +1,22 @@
+def load_task():
+    pass
+
+
+def save_task():
+    pass
+
+
+def view_task():
+    pass
+
+
+def create_task():
+    pass
+
+
+def mark_task_complete():
+    pass
+
+
+def main():
+    pass
